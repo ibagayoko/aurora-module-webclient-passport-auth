48105805be36561069bef1da596dac75b53dc569
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:23:56 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -1,38 +1,47 @@
 'use strict';
 
-module.exports = function (oAppData, iUserRole, bPublic) {
+module.exports = function (oAppData) {
 	var
 		_ = require('underscore'),
-		
+				
 		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
 				
+		App = require('%PathToCoreWebclientModule%/js/App.js'),
+		
 		Settings = require('modules/%ModuleName%/js/Settings.js'),
 		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {}),
 		
-		bAdminUser = iUserRole === Enums.UserRole.SuperAdmin,
-		bPowerUser = iUserRole === Enums.UserRole.NormalUser
+		bAdminUser = App.getUserRole() === Enums.UserRole.SuperAdmin,
+		bPowerUser = App.getUserRole() === Enums.UserRole.NormalUser
 	;
 
 	Settings.init(oSettings);
 	
-	return {
-		start: function (ModulesManager) {
-			if (bAdminUser)
-			{
+	if (bAdminUser)
+	{
+		return {
+			start: function (ModulesManager) {
 				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
 					function () { return require('modules/%ModuleName%/js/views/AdminSettingsView.js'); },
 					Settings.HashModuleName,
 					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
 				]);
 			}
-			if (bPowerUser)
-			{
+		};
+	}
+	
+	if (bPowerUser)
+	{
+		return {
+			start: function (ModulesManager) {
 				ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [
 					function () { return require('modules/%ModuleName%/js/views/UserSettingsView.js'); },
 					Settings.HashModuleName,
 					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
 				]);
 			}
-		}
-	};
+		};
+	}
+	
+	return null;
 };
