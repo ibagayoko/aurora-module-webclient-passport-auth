fe14242d992401120a83dba50e56085bf88289e3
nadine-shi <nadine@afterlogic.com>
2016-09-21 11:48:05 +0300

    FacebookAuthWebclient - copyright was added

--- module.php
+++ module.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * @copyright Copyright (c) 2016, Afterlogic Corp.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 class FacebookAuthWebclientModule extends AApiModule
 {
@@ -14,7 +30,13 @@ class FacebookAuthWebclientModule extends AApiModule
 		'OAuthIntegratorWebclient'
 	);
 	
-	public function init() 
+	/***** private functions *****/
+	/**
+	 * Initializes FacebookAuthWebclient Module.
+	 * 
+	 * @ignore
+	 */
+	public function init()
 	{
 		$this->incClass('connector');
 		$this->subscribeEvent('OAuthIntegratorAction', array($this, 'onOAuthIntegratorAction'));
@@ -23,7 +45,6 @@ class FacebookAuthWebclientModule extends AApiModule
 		$this->subscribeEvent('UpdateServicesSettings', array($this, 'onUpdateServicesSettings'));
 	}
 	
-	/***** private functions *****/
 	/**
 	 * Passes data to connect to service.
 	 * 
@@ -173,9 +194,9 @@ class FacebookAuthWebclientModule extends AApiModule
 		if ($oOAuthIntegratorWebclientDecorator)
 		{
 			$bResult = $oOAuthIntegratorWebclientDecorator->DeleteAccount($this->sService);
-		}		
+		}
 		
 		return $bResult;
-	}		
+	}
 	/***** public functions might be called with web API *****/
 }
