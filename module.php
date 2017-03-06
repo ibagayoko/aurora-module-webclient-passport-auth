3b66f61dd27d57d7c4814fa594a145f8f375b742
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:46:56 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\FacebookAuthWebclient;
 
-class FacebookAuthWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected $sService = 'facebook';
 	
