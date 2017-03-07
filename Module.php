6cda5c257db5f24ee7493df6b08418732501c57e
sash04ek <a.ovcharov@gmail.com>
2017-03-07 11:22:11 +0300

    added

--- Module.php
+++ Module.php
@@ -20,7 +20,7 @@
 
 namespace Aurora\Modules\FacebookAuthWebclient;
 
-class Module extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
 	protected $sService = 'facebook';
 	
